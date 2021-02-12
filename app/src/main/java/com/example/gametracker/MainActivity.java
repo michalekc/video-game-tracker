edfdf046873831a007bb3e9b75f2756ad3d6b889
michalekcm <42969890+michalekcm@users.noreply.github.com>
2021-02-12 00:54:50 -0600

    Layout for main screen started
    Made a layout using a ListView to display the console with a title and subtitle (but probably going to get rid of the sub) that are all clickable

--- app/src/main/java/com/example/gametracker/MainActivity.java
+++ app/src/main/java/com/example/gametracker/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.gametracker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int clickCount = 0;
+    ListView listView;
+    String mTitle[] = {"SNES", "NES", "N64", "Gamecube", "PlayStation 5"};
+    String mDesc[] = {"Super Nintendo", "Nintendo Entertainment System", "Nintendo 64", "Nintendo Gamecube", "Sony Playstation 5"};
+    int images[] = {R.drawable.snes, R.drawable.nes, R.drawable.n64,R.drawable.gamecube, R.drawable.ps5};
+
 
 
     @Override
@@ -16,17 +29,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView = findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDesc, images);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0) {
+                    //show SNES collection page
+                }
+                if(position == 1) {
+                    //show NES collection page
+                }
+                if(position == 2) {
+                    //show N64 collection page
+                }
+                if(position == 3) {
+                    //show Gamecube collection page
+                }
+                if(position == 4) {
+                    //show GBA collection page
+                }
+            }
+        });
     }
 
 
-    public void testButton(View view) {
-        TextView mainText = (TextView)findViewById(R.id.mainText);
-        if(clickCount == 0) {
-            mainText.setText("Test 1!");
-            clickCount = 1;
-        } else {
-            mainText.setText("Test 2!");
-            clickCount = 0;
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter(Context c, String title[], String desc[], int imgs[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = desc;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater =(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView myDesc = row.findViewById(R.id.textView2);
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDesc.setText(rDescription[position]);
+            return row;
         }
     }
 }
